28f64394ce6b506b20587619acfe608af82298c2
joelBonelli <joel.bonelli@davinci.edu.ar>
2025-04-10 20:40:37 -0300

    modificacion DB

--- includes/database.php
+++ includes/database.php
@@ -11,7 +11,7 @@ $db->set_charset('utf8');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_errno();
+    echo "errno de depuración: " . mysqli_connect_error();
     echo "error de depuración: " . mysqli_connect_error();
     exit;
 }
